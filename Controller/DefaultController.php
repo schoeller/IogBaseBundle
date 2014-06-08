3b52900e6f9612bc12b2d9873dc72cf4979db511
Sebastian Schoeller <sebastian.schoeller@gmail.com>
2014-06-08 23:49:04 +0000

    First commit of bundle

---
+++ Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Iog\BaseBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction($name)
+    {
+        return $this->render('IogBaseBundle:Default:index.html.twig', array('name' => $name));
+    }
+}
---
+++ IogBaseBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Iog\BaseBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class IogBaseBundle extends Bundle
+{
+}
